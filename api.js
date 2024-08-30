525b4e6bc1435d06bbce1a354efaa58783cc0ed5
Lica Simon <carvalho.heloisa@escola.pr.gov.br>
2024-08-30 11:28:39 -0300

    first commit

--- api.js
+++ api.js
@@ -1,8 +1,6 @@
-//consultar tarefa por id
-//atualizar o status da tarefa 
+// eliminar tasks existentes
+//incerir uma nova taks
 
-
-//1 requires
 const express = require('express');
 const mysql = require('mysql2');
 const cors = ('cors');
@@ -10,18 +8,14 @@ const cors = ('cors');
 const mysql_config = require('/inc/mysql_config');
 const functions = require('/inc/functions');
 
-//2 criação de duas constatntes para definição da disponibilidade da
-// api e da versão da api
 const API_AVAILABILITY = true;
-const API_VERSION = '2.0.0';
+const API_VERSION = '3.0.0';
 
-//3 iniciar o server
 const app = express();
 app.listen(3000,()=>{
     console.log("API está ativo")
 })
 
-//4 checar se API está disponivel 
 app.use((req,res,next)=>{
     if (API_AVAILABILITY){
         next();
@@ -30,24 +24,31 @@ app.use((req,res,next)=>{
     }
 })
 
-//5 mysql connection
 const connection = mysql.createConnection(mysql_config);
 
-//6 cors
 app.use(cors());
 
-//7 rotas
-//rota inicial que vai dizer que a API está disponível
+//tratamento dos posts params
+app.use(json());
+//instrução que pede que o express trate os dados como um json
+
+app.use(express.urlencoded({extended: true}));
+//quando é enviado um pedido através do método post, os dados enviados
+//através de um formulário podem ser interpretados
+//SEM ESSES DOIS MIDLEWERE NÃO SERIA POSSÍVEL BUSCAR OS PARÂMETROS
+
+//rotas
+//rota de entrada
 app.get('/', (req,res)=>{
     res.json(functions.response('sucesso', 'API está rodando',0, null))
 })
 
-//9 rota para chegar em todas as tarefas 
+//rota pra pegar todas as tasks
 app.get('/tasks',(req,res)=>{
     connection.query('SELECT * FROM tasks', (err,rows))
 })
 
-//10 rota para pegar a task pelo id
+//rota pra pegar as taks pelo id
 app.get('/tasks/:id', (req,res)=>{
     const id = req.params.id;
     connection.query('SELECT * FROM tasks WHERE id=?', [id], (err, rows)=>{
@@ -65,7 +66,7 @@ app.get('/tasks/:id', (req,res)=>{
     })
 })
 
-//11 atualizar o status de uma task - método 
+//rota atualizar o status de uma task - método put
 app.put('/tasks/:id/status/:status', (req,res)=>{
     const id = req.params.id;
     const status = req.params.status;
@@ -81,7 +82,52 @@ app.put('/tasks/:id/status/:status', (req,res)=>{
     })
 })
 
-//8 midleware para caso alguma frota não seja encontrada
+//rota para deletar uma tarefa 
+app.delete('/tasks/:id/delete', (req,res)=>{
+    const id = req.params.id;
+    connection.query('DELETE FROM tasks WHERE id =?', [id], (err,rows)=>{
+        if(!err){
+            if(rows.affectedRows>0){
+                res.json(functions.response('Sucesso', 'Task deletada', rows.affectedRows, null))
+            }
+            else{
+                res.json(functions.response('Atenção', 'Task não encontrada', 0, null))
+            }   
+        }
+        else{
+            res.json(functions.response('Erro', err.message, 0, null))
+        }
+
+    })
+})
+
+//rota para inserir uma nova task
+app.put('/tasks/create', (req,res)=>{
+    //midleware para a recepção dos dados da tarefa(task)
+
+    //pegando os dados da request
+    const post_data = req.body;
+    //checar para ver se não estamos recebendo uma json vazia
+    if(post_data ==undefined){
+        res.json(functions.response('Atenção', 'Sem dados de uma nova task', 0, null))
+        return;
+    } 
+    const taks = post_data.taks;
+    const status = post_data.status;
+
+
+    //inserindo a nova task
+    connection.query('INSERT INTO tasks(task,status,created_at) VALUES(?, ?, NOW(), NOW()', [task,status], (err,rows)=>{
+        if(!err){
+            res.json('Sucesso', 'Task cadastrada no banco', rows.affectedRows, null)
+        }
+        else{
+            res.json(functions.response('Erro', err.message, 0, null))
+        }
+    })
+})
+
+//midleware para caso alguma frota não seja encontrada
 app.use((req,res)=>{
     res.json(functions.response('atenção', 'rota não encontrada',0, null))
 })
